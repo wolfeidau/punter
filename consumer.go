74b8580781cde2ad51d7066fd7f463778908512b
Mark Wolfe <mark@wolfe.id.au>
2015-02-09 10:58:25 +1100

    Changed to use loggo logger.

--- consumer.go
+++ consumer.go
@@ -2,9 +2,13 @@ package punter
 
 import (
 	"fmt"
-	"log"
 
+	"github.com/juju/loggo"
 	"github.com/streadway/amqp"
+)
+
+var (
+	log = loggo.GetLogger("punter")
 )
 
 // MsgHander function which is supplied to handle delivers.
@@ -42,23 +46,23 @@ func NewConsumer(config *Config, ctag string, msgHandler MsgHander) (*Consumer, 
 
 	var err error
 
-	log.Printf("dialing %q", config.AmqpURI)
+	log.Infof("dialing %q", config.AmqpURI)
 	c.conn, err = amqp.Dial(config.AmqpURI)
 	if err != nil {
 		return nil, fmt.Errorf("Dial: %s", err)
 	}
 
 	go func() {
-		fmt.Printf("closing: %s", <-c.conn.NotifyClose(make(chan *amqp.Error)))
+		log.Infof("closing: %s", <-c.conn.NotifyClose(make(chan *amqp.Error)))
 	}()
 
-	log.Printf("got Connection, getting Channel")
+	log.Infof("got Connection, getting Channel")
 	c.channel, err = c.conn.Channel()
 	if err != nil {
 		return nil, fmt.Errorf("Channel: %s", err)
 	}
 
-	log.Printf("got Channel, declaring Exchange (%q)", config.Exchange)
+	log.Infof("got Channel, declaring Exchange (%q)", config.Exchange)
 	if err = c.channel.ExchangeDeclare(
 		config.Exchange,     // name of the exchange
 		config.ExchangeType, // type
@@ -77,7 +81,7 @@ func NewConsumer(config *Config, ctag string, msgHandler MsgHander) (*Consumer, 
 		qargs["x-message-ttl"] = config.MessageTTL
 	}
 
-	log.Printf("declared Exchange, declaring Queue %q", config.QueueName)
+	log.Infof("declared Exchange, declaring Queue %q", config.QueueName)
 	queue, err := c.channel.QueueDeclare(
 		config.QueueName, // name of the queue
 		true,             // durable
@@ -90,7 +94,7 @@ func NewConsumer(config *Config, ctag string, msgHandler MsgHander) (*Consumer, 
 		return nil, fmt.Errorf("Queue Declare: %s", err)
 	}
 
-	log.Printf("declared Queue (%q %d messages, %d consumers), binding to Exchange (key %q)",
+	log.Infof("declared Queue (%q %d messages, %d consumers), binding to Exchange (key %q)",
 		queue.Name, queue.Messages, queue.Consumers, config.Key)
 
 	args := amqp.Table{}
@@ -107,7 +111,7 @@ func NewConsumer(config *Config, ctag string, msgHandler MsgHander) (*Consumer, 
 		return nil, fmt.Errorf("Queue Bind: %s", err)
 	}
 
-	log.Printf("Queue bound to Exchange, starting Consume (consumer tag %q)", c.tag)
+	log.Infof("Queue bound to Exchange, starting Consume (consumer tag %q)", c.tag)
 	deliveries, err := c.channel.Consume(
 		queue.Name, // name
 		c.tag,      // consumerTag,
@@ -137,7 +141,7 @@ func (c *Consumer) Shutdown() error {
 		return fmt.Errorf("AMQP connection close error: %s", err)
 	}
 
-	defer log.Printf("AMQP shutdown OK")
+	defer log.Infof("AMQP shutdown OK")
 
 	// wait for handle() to exit
 	return <-c.done
